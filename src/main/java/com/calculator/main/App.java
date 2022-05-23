50c4ff9e73af7df3ff9693ca273fab53cd7ef1d1
Manuel Pineda <pineda.manuel@gmail.com>
2022-05-23 11:28:57 -0500

    Start functional programming calculator

---
+++ src/main/java/com/calculator/main/App.java
@@ -0,0 +1,7 @@
+package main.java.com.calculator.main;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello world!!");
+    }
+}
---
+++ src/main/java/com/calculator/operations/ISumNumbers.java
@@ -0,0 +1,4 @@
+package main.java.com.calculator.operations;
+
+public interface ISumNumbers {
+}

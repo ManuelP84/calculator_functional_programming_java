b0bd60aee84372356473d0f32a7eff1d3caa64d1
Manuel Pineda <pineda.manuel@gmail.com>
2022-05-23 19:46:59 -0500

    Division and multiplication implemented.

--- src/main/java/com/calculator/main/App.java
+++ src/main/java/com/calculator/main/App.java
@@ -2,27 +2,34 @@ package main.java.com.calculator.main;
 
 import main.java.com.calculator.operations.IOperation;
 
+import java.util.stream.Stream;
+
 public class App {
     public static void main(String[] args) {
         System.out.println("****************************************************");
         System.out.println("Welcome to the Functional Programming Calculator");
         System.out.println("****************************************************");
 
-        IOperation sumImplementation = (num1, num2) -> num1 + num2;
-        IOperation substractImplementation = ((num1, num2) -> num1 - num2);
 
-        var sumResult = executeSum(sumImplementation);
-        System.out.println(sumResult);
+        IOperation adition = (num1, num2) -> num1 + num2;
+        IOperation substraction = ((num1, num2) -> num1 - num2);
+        IOperation multiply = (a, b) -> Stream.iterate(a, n -> adition.operate(0, a))
+                .limit(b).reduce(0, (n, m) -> n + m);
+        IOperation division = (a, b) -> Stream
+                .iterate(a, n -> n >= 0, n -> b == 0 ?  -1 : substraction.operate(n, b))
+                .toList()
+                .size()-1;
 
-        var substractResult = executeSubstraction(substractImplementation);
-        System.out.println(substractResult);
-    }
+        var aditionResult = adition.operate(5,5);
+        System.out.println("Sum: " + aditionResult);
 
-    static Integer executeSum(IOperation sum){
-        return sum.operate(5,5);
-    }
+        var substractResult = substraction.operate(10,5);
+        System.out.println("Substraction: " + substractResult);
 
-    static Integer executeSubstraction(IOperation substraction){
-        return substraction.operate(8,4);
+        var multiplication = multiply.operate(1,5);
+        System.out.println("Multiplication: "+ multiplication);
+
+        var divisionResult = division.operate(10,2);
+        System.out.println("Division: "+ divisionResult);
     }
 }
